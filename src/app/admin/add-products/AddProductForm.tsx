92cc71fcbf20c7337a512209f52fc827c049ae92
pelumini <pelumini@gmail.com>
2024-07-15 21:35:20 +0100

    custom inputs added

--- src/app/admin/add-products/AddProductForm.tsx
+++ src/app/admin/add-products/AddProductForm.tsx
@@ -1,9 +1,121 @@
 "use client";
 
-import React from "react";
+import Heading from "@/components/Heading";
+import CategoryInput from "@/components/inputs/CategoryInput";
+import CustomCheckBox from "@/components/inputs/CustomCheckBox";
+import Input from "@/components/inputs/Input";
+import TextArea from "@/components/inputs/TextArea";
+import { categories } from "@/utils/categories";
+import React, { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const AddProductForm = () => {
-  return <div>AddProductForm</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      description: "",
+      brand: "",
+      category: "",
+      inStock: false,
+      images: [],
+      price: "",
+    },
+  });
+
+  const category = watch("category");
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
+
+  return (
+    <>
+      <Heading title="Add a Product" center />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="price"
+        label="Price"
+        disabled={isLoading}
+        register={register}
+        type="number"
+        errors={errors}
+        required
+      />
+      <Input
+        id="brand"
+        label="Brand"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <TextArea
+        id="description"
+        label="Description"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <CustomCheckBox
+        id="inStock"
+        register={register}
+        label="This product is in stock"
+      />
+      <div className="w-full font-medium">
+        <div className="mb-2 font-semibold">Select a Category</div>
+        <div className="grid grid-cols-2 md:grid-cols-3 max-h-[50vh] gap-3 overflow-y-auto">
+          {categories.map((item) => {
+            if (item.label === "All") {
+              return null;
+            }
+
+            return (
+              <div key={item.label} className="col-span">
+                <CategoryInput
+                  onClick={(category) => setCustomValue("category", category)}
+                  selected={category === item.label}
+                  label={item.label}
+                  icon={item.icon}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="w-full flex flex-col flex-wrap gap-4">
+        <div>
+          <div className="font-bold">
+            Select the available product colors and upload their images
+          </div>
+          <div className="text-sm">
+            You must upload an image for each of the color selected otherwise
+            your color selection will be ignored
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default AddProductForm;
